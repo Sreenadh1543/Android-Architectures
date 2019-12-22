8b10c721430bd141e528fe3474df4b4e5c4adf37
sreenadh1543@gmail.com <sreenadh1543@gmail.com>
2019-12-22 22:03:51 +0100

    Added model for countries list

---
+++ app/src/main/java/com/example/androidarchitectures/model/Country.java
@@ -0,0 +1,9 @@
+package com.example.androidarchitectures.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Country {
+
+    @SerializedName("name")
+    private String countryName;
+}
