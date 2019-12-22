c303d84cce1acaf2d86ae65550e5541ce2136080
sreenadh1543@gmail.com <sreenadh1543@gmail.com>
2019-12-22 22:06:23 +0100

    Added caller for Countries API with retrofit

---
+++ app/src/main/java/com/example/androidarchitectures/model/CountryService.java
@@ -0,0 +1,17 @@
+package com.example.androidarchitectures.model;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class CountryService {
+    public static final String BASE_URL="https://restcountries.eu/rest/v2/";
+    private CountriesApi api;
+    public CountryService(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build();
+    }
+}
