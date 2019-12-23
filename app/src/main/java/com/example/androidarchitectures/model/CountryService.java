bbf2c34b197df6aa67a0563deb459e2fbaee5431
Sreenadh1543 <sreenadh1543@gmail.com>
2019-12-23 18:35:01 +0100

    Feat : Added method to expose country list Api
    Method to Expose country list as a utility for MVC , MVP and MVVM Architectures.

--- app/src/main/java/com/example/androidarchitectures/model/CountryService.java
+++ app/src/main/java/com/example/androidarchitectures/model/CountryService.java
@@ -1,5 +1,8 @@
 package com.example.androidarchitectures.model;
 
+import java.util.List;
+
+import io.reactivex.Single;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -13,5 +16,10 @@ public class CountryService {
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
+        api=retrofit.create(CountriesApi.class);
+    }
+
+    public Single<List<Country>> getCountries(){
+        return api.getCountries();
     }
 }
