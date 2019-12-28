4a09ba0890485cd705b8a90dcb15f6742c74281d
sreenadh1543@gmail.com <sreenadh1543@gmail.com>
2019-12-28 19:57:11 +0100

    feat : Add Getters and Setters to country Names

--- app/src/main/java/com/example/androidarchitectures/model/Country.java
+++ app/src/main/java/com/example/androidarchitectures/model/Country.java
@@ -6,4 +6,12 @@ public class Country {
 
     @SerializedName("name")
     private String countryName;
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
 }
